ef23cc5105bf0006658f20e846d792242ee8040a
VitaliyYakovenko <yakovenkovitaliy1@gmail.com>
2026-01-26 22:09:33 +0200

    add

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 const Home =() => import("../pages/Home.vue");
 const ContactDetail = () => import("../pages/ContactDetail.vue");
+const FormPage = () => ("../pages/FormPage.vue");
 
 
 const routes = [
